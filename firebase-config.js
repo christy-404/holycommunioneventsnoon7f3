9df34d0dfa2784ed0e0bc09ec8d6ff416adc8d0a
christy-404 <christianoskoodathinkal@gmail.com>
2026-04-26 00:17:34 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+/**
+ * Firebase Configuration
+ * PASTE YOUR FIREBASE CONFIG OBJECT BELOW (from Firebase Console)
+ */
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
